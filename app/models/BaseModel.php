250ef2852927452104adb4baf1a6cea8827db758
bogilas <delije.epicentar@gmail.com>
2018-09-04 03:13:02 +0200

    baseModel-fixes

--- app/models/BaseModel.php
+++ app/models/BaseModel.php
@@ -13,15 +13,15 @@ class BaseModel{
     }
 
     public static function getCPCompanie($id){
-        $SQL = "SELECT P.pun_naziv, P.kratak_naziv, P.mat_br, P.pib, P.sajt_link, P.telefon, P.posebne_napomene, P.logotip, P.kratak_opis, D.Naziv as glavna_delatnost, L.adresa,O.naziv as opstina_naziv, G.naziv as grad_naziv, R.naziv as regija_naziv, L.kordinata_duzina, L.kordinata_sirina  FROM preduzece P, delatnosti D, lokacija L, opstina O, grad G, regija R WHERE P.status = 1 AND P.preduzece_sif = ? AND P.glavna_delatnost_sif = D.delatnosti_SIF AND p.glavna_lokacija_sif = L.lokacija_sif AND L.opstina_sif = O.opstina_sif AND O.grad_sif = G.grad_sif AND G.regija_sif = R.regija_sif"
+        $SQL = "SELECT P.pun_naziv, P.kratak_naziv, P.mat_br, P.pib, P.sajt_link, P.telefon, P.posebne_napomene, P.logotip, P.kratak_opis, D.Naziv as glavna_delatnost, L.adresa,O.naziv as opstina_naziv, G.naziv as grad_naziv, R.naziv as regija_naziv, L.kordinata_duzina, L.kordinata_sirina  FROM preduzece P, delatnosti D, lokacija L, opstina O, grad G, regija R WHERE P.status = 1 AND P.preduzece_sif = ? AND P.glavna_delatnost_sif = D.delatnosti_SIF AND p.glavna_lokacija_sif = L.lokacija_sif AND L.opstina_sif = O.opstina_sif AND O.grad_sif = G.grad_sif AND G.regija_sif = R.regija_sif";
         $prep = DataBase::getInstance()->prepare($SQL);
         $res = $prep->execute([$id]);
         $data = [];// = array();
         if($res){
             $basic = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["basic_data" => $basic];
+            $data["basic_data"] = $basic;
         }else{
-            $data["error" => "Nije nadjeno preduzece"];
+            $data["error" ] = "Nije nadjeno preduzece";
             return $data;
         }
         
@@ -30,9 +30,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $additional_telephones = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["additional_telephones" => $additional_telephones];
+            $data["additional_telephones"] = $additional_telephones;
         }else{
-            $data["additional_telephones" => []];
+            $data["additional_telephones"] = [];
         }
         
         $SQL = "SELECT S.slika FROM slike S where S.preduzece_sif = ?";
@@ -40,9 +40,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $images = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["images" => $images];
+            $data["images"] = $images;
         }else{
-            $data["images" => []];
+            $data["images"] = [];
         }
 
         $SQL = "SELECT RV.day,RV.otvara,RV.zatvara FROM radno_vreme RV WHERE RV.preduzece_sif = ?";
@@ -50,9 +50,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $work_days = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["work_days" => $work_days];
+            $data["work_days"] = $work_days;
         }else{
-            $data["work_days" => []];
+            $data["work_days"] = [];
         }
 
         $SQL = "SELECT D.sifra,D.Naziv FROM delatnosti D, preduzece_delatnost PD WHERE PD.preduzece_sif = ? AND PD.delatnost_sif = D.delatnosti_SIF";
@@ -60,9 +60,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $work_type = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["work_type" => $work_type];
+            $data["work_type"] = $work_type;
         }else{
-            $data["work_type" => []];
+            $data["work_type"] = [];
         }
 
         $SQL = "SELECT L.adresa,L.kordinata_duzina,L.kordinata_sirina,O.naziv as opstina_naziv, G.naziv as grad_naziv, R.naziv as regija_naziv FROM lokacija L, opstina O, grad G, regija R WHERE L.preduzece_sif = ? AND L.opstina_sif=O.opstina_sif AND O.grad_sif = G.grad_sif AND G.regija_sif = R.regija_sif";
@@ -70,9 +70,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $locations = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["locations" => $work_type];
+            $data["locations"] = $work_type;
         }else{
-            $data["locations" => []];
+            $data["locations"] => [];
         }
 
         $SQL = "SELECT NP.cena,P.naziv,P.opis,VP.naziv as naziv_vrsta_proizvoda FROM nudi_proizvod NP, proizvod P, vrsta_proizvoda VP WHERE NP.preduzece_sif = ? AND NP.proizvod_sif=P.proizvod_sif AND P.vrsta_proizvoda_sif = VP.vrsta_proizvoda_sif";
@@ -80,9 +80,9 @@ class BaseModel{
         $res = $prep->execute([$id]);
         if($res){
             $products = $prep->fetchAll(PDO::FETCH_OBJ);
-            $data["products" => $products];
+            $data["products"] = $products;
         }else{
-            $data["products" => []];
+            $data["products"] = [];
         }
         return $data;
     }
