a8673c9ba727cebfaa837843e323608f81af7688
jiggyBu <igor.stric@gmail.com>
2018-09-04 03:33:06 +0200

    views mocks, basic controller

---
+++ app/views/Main/preduzeca.php
@@ -0,0 +1,92 @@
+<?php include 'app/views/_global/beforeContent.php'; ?>
+
+    <!-- Main Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="col-lg-4 col-md-4 float-left row">
+            <span>Hey ho</span>
+          </div>
+          <div class="col-lg-8 col-md-8 float-left row">
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-bottom-margin">
+              <div class="ads-item">
+                <a href="#">
+                    <div class="ads-logo">
+                      <div class="ads-logo-bg-blur"></div>
+                      <img src="" alt="TODO">
+                    </div>
+                    <div class="ads-intermid">
+                      <h2 class="ads-title">TODO</h2>
+                      <div class="ads-address">
+                        TODO TODO TODO TODO
+                      </div>
+                    </div>
+                    <div class="ads-desc">
+                      TODO TODO TODO TODO TODO TODO TODO TODO TODO
+                    </div>
+                  </a>
+                </div>
+              </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-bottom-margin">
+              <div class="ads-item">
+                <a href="#">
+                    <div class="ads-logo">
+                      <div class="ads-logo-bg-blur"></div>
+                      <img src="" alt="TODO">
+                    </div>
+                    <div class="ads-intermid">
+                      <h2 class="ads-title">TODO</h2>
+                      <div class="ads-address">
+                        TODO TODO TODO TODO
+                      </div>
+                    </div>
+                    <div class="ads-desc">
+                      TODO TODO TODO TODO TODO TODO TODO TODO TODO
+                    </div>
+                  </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-bottom-margin">
+              <div class="ads-item">
+                <a href="#">
+                    <div class="ads-logo">
+                      <div class="ads-logo-bg-blur"></div>
+                      <img src="" alt="TODO">
+                    </div>
+                    <div class="ads-intermid">
+                      <h2 class="ads-title">TODO</h2>
+                      <div class="ads-address">
+                        TODO TODO TODO TODO
+                      </div>
+                    </div>
+                    <div class="ads-desc">
+                      TODO TODO TODO TODO TODO TODO TODO TODO TODO
+                    </div>
+                  </a>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 col-bottom-margin">
+              <div class="ads-item">
+                <a href="#">
+                    <div class="ads-logo">
+                      <div class="ads-logo-bg-blur"></div>
+                      <img src="" alt="TODO">
+                    </div>
+                    <div class="ads-intermid">
+                      <h2 class="ads-title">TODO</h2>
+                      <div class="ads-address">
+                        TODO TODO TODO TODO
+                      </div>
+                    </div>
+                    <div class="ads-desc">
+                      TODO TODO TODO TODO TODO TODO TODO TODO TODO
+                    </div>
+                  </a>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<?php include 'app/views/_global/afterContent.php'; ?>
